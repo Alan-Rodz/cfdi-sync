00edd416fb934fbfe2b3ae119732e3ca18d5c302
Alan Rodz <s061199@hotmail.com>
2026-04-18 19:01:02 -0600

    feat: add environment configuration files and utility functions for error logging

---
+++ package/common/src/util/error.ts
@@ -0,0 +1,8 @@
+// ********************************************************************************
+export const logConsoleError = (error: unknown, metadata: string) => {
+ if (error instanceof Error) {
+  console.error(`${metadata}: ${error.message}`);
+ } /* else -- not an error */
+
+ console.error(`${metadata}: ${String(error)}`);
+}
