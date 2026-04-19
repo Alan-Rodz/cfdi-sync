0b6223832ea1591cbd4274c6ab7b8cce39c26a47
Alan Rodz <s061199@hotmail.com>
2026-04-19 13:22:46 -0600

    feat: implement logout functionality and update route handling

--- package/frontend/src/route/logout.tsx
+++ package/frontend/src/route/logout.tsx
@@ -1,9 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react';
 
-export const Route = createFileRoute('/logout')({
-  component: RouteComponent,
-})
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 
-function RouteComponent() {
-  return <div>Hello "/logout"!</div>
-}
+import { frontendRoutes } from 'common';
+
+import { ensureProfileIs } from './guard';
+
+import { useAuth } from '@/ui/hook/useAuth';
+
+// ********************************************************************************
+// == Component ===================================================================
+const LogoutPage = () => {
+ const navigate = useNavigate();
+ const { logout } = useAuth();
+
+ // -- Effect ---------------------------------------------------------------------
+ useEffect(() => {
+  logout();
+  navigate({ to: frontendRoutes.nonAuthed.landing_page });
+ }, [logout, navigate]);
+
+ return null;
+};
+
+// == Export ======================================================================
+export const Route = createFileRoute(frontendRoutes.nonAuthed.logout)({
+ beforeLoad: ensureProfileIs('loggedIn', frontendRoutes.nonAuthed.landing_page),
+ component: LogoutPage,
+});
